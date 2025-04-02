9780f78ba0e97866e0b28dc56680a058bd094c1e
Flávio Melo <flavioovatsug@gmail.com>
2025-04-02 17:05:43 -0300

    feat(backend): Atualiza mcp e altera a nomenclatura dos codigos mcp

---
+++ backend/mcp_server.py
@@ -0,0 +1,17 @@
+from mcp.server.fastmcp import FastMCP
+import asyncio
+
+mcp = FastMCP("Teste")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Soma dois números"""
+    return a + b
+
+@mcp.tool()
+def to_uppercase(text: str) -> str:
+    """Converte texto para maiúsculas"""
+    return text.upper()
+
+if __name__ == "__main__":
+    asyncio.run(mcp.run(transport="sse"))
