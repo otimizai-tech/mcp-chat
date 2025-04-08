4e5574d7da1821897f5e53144d213c245f404e74
Flávio Melo <flavioovatsug@gmail.com>
2025-04-08 17:24:35 -0300

    feat(backend): Cria novo servidor pra integrar com copilotkit

---
+++ backend/dev_server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from copilotkit.integrations.fastapi import add_fastapi_endpoint
+from copilotkit import CopilotKitRemoteEndpoint, LangGraphAgent
+from mcp_client import graph
+
+
+app = FastAPI()
+sdk = CopilotKitRemoteEndpoint(
+    agents=[
+        LangGraphAgent(
+            name="mcp_agent",
+            description="This agent tha uses mcp servers to run tools",
+            graph=graph,
+        )
+    ],
+)
+
+add_fastapi_endpoint(app, sdk, "/copilotkit")
+
+def main():
+    """Run the uvicorn ."""
+    import uvicorn
+    uvicorn.run("dev_server:app", host="0.0.0.0", port=8002, reload=True)
+ 
+if __name__ == "__main__":
+    main()
